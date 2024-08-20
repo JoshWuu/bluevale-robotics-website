20cd25dd63024c4ed9e2cc0939bbbcd6ed3e0564
JoshWuu <joshspwu@gmail.com>
2024-08-20 12:45:09 -0400

    background

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,8 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Projects',
+  description: 'Learn mroe about past projects.',
 }
 
 export default function Page() {
